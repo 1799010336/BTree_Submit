fc2c667c9e5f9ef0d558bae90d902840d246791e
1799010336 <48859030+1799010336@users.noreply.github.com>
2019-06-21 19:11:26 +0800

    Update BTree.hpp

--- BTree.hpp
+++ BTree.hpp
@@ -17,15 +17,22 @@
 
 #include <cstdio>
 namespace sjtu {
-
+    template <class Key,class Value>
+    constexpr long int getblocksize()
+    {
+		long int cursize=4096;
+		while (cursize<(sizeof(Key)+sizeof(Value))*8)
+            cursize<<=2;
+		return cursize;
+    }
     constexpr char address[128]="bptree_data.sjtu";//bpt地址
     template <class Key, class Value, class Compare = std::less<Key> >
 
     class BTree {
-
+friend class iterator;
+friend class const_iterator;
 public:
 
-        constexpr static long int blocksize=4096;
         struct blockhead{
         bool type;//是否为叶节点
         long int _size;//大小
@@ -40,6 +47,8 @@ public:
             long int child=0;//儿子节点
             Key key;//键值
         };
+
+        constexpr static long int blocksize=getblocksize<Key,Value>();
         constexpr static long int keysize=sizeof(Key);//键的大小
         constexpr static long int blockheadsize=sizeof(blockhead);//块头大小
         constexpr static long int valuesize=sizeof(Value);//value大小
@@ -67,7 +76,7 @@ public:
         FILE* fp;
         //读入内存
         template <class T>
-        inline void readmem(T buffer,long int buffersize,long int pos)
+        void readmem(T buffer,long int buffersize,long int pos)
         {
             fseek(fp,pos*buffersize,SEEK_SET);//文件流、偏移量、起始位置
             fread(buffer,buffersize,1,fp);//读取对象，大小，数量，文件流
@@ -75,7 +84,7 @@ public:
         }
         //读取内存
         template <class T>
-        inline void writemem(T buffer,long int buffersize,long int pos)
+        void writemem(T buffer,long int buffersize,long int pos)
         {
             fseek(fp,pos*buffersize,SEEK_SET);
             fwrite(buffer,buffersize,1,fp);
@@ -91,7 +100,7 @@ public:
         }
         //写入节点
         template <class T>
-        inline void writeblock(blockhead *head,T *data,long int pos)
+        void writeblock(blockhead *head,T *data,long int pos)
         {
             char buffer[blocksize]={0};
             memcpy(buffer,head,blockheadsize);
@@ -127,7 +136,7 @@ public:
             writeblock(&tmp,&node,position);
             return position;
         }
-        long int addleafnode(long int pa,long int next,long int prev)
+        long int addleafnode(long int pa,long int prev,long int next)
         {
             long int position=memallocate();
             blockhead tmp;
@@ -304,129 +313,255 @@ public:
     public:
 
         typedef pair<const Key, Value> value_type;
-
-
-
-
-
         class const_iterator;
+		class iterator {
+			friend class BTree;
 
-        class iterator {
+		private:
+			// Your private members go here
+			BTree* curtree;
+			blockhead block_head;
+			long int curpos;
 
-       // private:
-
-           // Your private members go here
-
-       public:
-            int nothing;
-            iterator():nothing(0){}
-           /* bool modify(const Value& value){
-
+		public:
 
 
-            }
+			iterator()
+			{
+				curtree=nullptr;
+				curpos=0;
+			}
+			iterator(const iterator& other)
+			{
+				// TODO Copy Constructor
+				curtree = other.curtree;
+				block_head = other.block_head;
+				curpos = other.curpos;
+			}
+			bool modify(const Value& value)
+			{
+				blockhead head;
+				leafnode leaf_node;
+				readblock(&head, &leaf_node, block_head.pos);
+				leaf_node.val[curpos].second = value;
+				writeblock(&head, &leaf_node, block_head.pos);
+				return true;
+			}
+			// Return a new iterator which points to the n-next elements
+			iterator operator++(int)
+			{
+				// Todo iterator++
+				long int tmp=*this;
+				++curpos;
+				if (curpos>=block_head._size)
+                {
+					char buffer[blocksize];
+					curtree->readmem(buffer,blocksize,block_head.next);
+					memcpy(&block_head,buffer,sizeof(block_head));
+					curpos=0;
+				}
+				return tmp;
+			}
+			iterator& operator++() {
+				// Todo ++iterator
+				++curpos;
+				if (curpos>=block_head._size) {
+					char buffer[blocksize];
+					curtree->readmem(buffer,blocksize,block_head.next);
+					memcpy(&block_head,buffer,sizeof(block_head));
+					curpos=0;
+				}
+				return *this;
+			}
+			iterator operator--(int)
+			{
+				// Todo iterator--
+				long int tmp=*this;
+				if (curpos==0) {
+					char buffer[blocksize];
+					curtree->readmem(buffer,blocksize,block_head.prev);
+					memcpy(&block_head,buffer,sizeof(block_head));
+					curpos=block_head._size-1;
+				}
+				else --curpos;
+				return tmp;
+			}
+			iterator& operator--()
+			{
+				// Todo --iterator
+				if (curpos==0) {
+					char buffer[blocksize];
+					curtree->readmem(buffer,blocksize,block_head.prev);
+					memcpy(&block_head,buffer,sizeof(block_head));
+					curpos=block_head._size-1;
+				}
+				else --curpos;
 
-            iterator() {
+				return *this;
+			}
+			// Overloaded of operator '==' and '!='
+			// Check whether the iterators are same
+			value_type operator*() const
+			{
+				// Todo operator*, return the <K,V> of iterator
+				if (curpos>=block_head._size) throw invalid_iterator();
+				char buffer[blocksize];
+				curtree->readmem(buffer,blocksize,block_head.pos);
+				leafnode leaf_node;
+				memcpy(&leaf_node,buffer+blockheadsize,sizeof(leaf_node));
+				value_type re(leaf_node.val[curpos].first,leaf_node.val[curpos].second);
+				return re;
+			}
+			bool operator==(const iterator& rhs) const
+			{
 
-                // TODO Default Constructor
+				return (curtree==rhs.curtree&&block_head.pos==rhs.block_head.pos&&curpos==rhs.curpos);
+			}
+			bool operator==(const const_iterator& rhs) const
+			{
 
-            }
+				return (curtree==rhs.curtree&&block_head.pos==rhs.block_head.pos&&curpos==rhs.curpos);
+			}
+			bool operator!=(const iterator& rhs) const
+			{
 
-            iterator(const iterator& other) {
+				return !(*this==rhs);
+			}
+			bool operator!=(const const_iterator& rhs) const
+			{
 
-                // TODO Copy Constructor
-
-            }
-
-            // Return a new iterator which points to the n-next elements
-
-            iterator operator++(int) {
-
-                // Todo iterator++
-
-            }
-
-            iterator& operator++() {
-
-                // Todo ++iterator
-
-            }
-
-            iterator operator--(int) {
-
-                // Todo iterator--
-
-            }
-
-            iterator& operator--() {
-
-                // Todo --iterator
-
-            }
-
-            // Overloaded of operator '==' and '!='
-
-            // Check whether the iterators are same
-
-            bool operator==(const iterator& rhs) const {
-
-                // Todo operator ==
-
-            }
-
-            bool operator==(const const_iterator& rhs) const {
-
-                // Todo operator ==
-
-            }
-
-            bool operator!=(const iterator& rhs) const {
-
-                // Todo operator !=
-
-            }
-
-            bool operator!=(const const_iterator& rhs) const {
-
-                // Todo operator !=
-
-            }*/
-
-        };
+				return !(*this==rhs);
+			}
+		};
 
         class const_iterator {
-
-            // it should has similar member method as iterator.
-
-            //  and it should be able to construct from an iterator.
-
-       //private:
-
-            // Your private members go here
+        friend class BTree;
+        private:
+            BTree* curtree;
+			blockhead block_head;
+			long int curpos;
 
         public:
-            int nothing;
-            const_iterator():nothing(0){}
-           /* const_iterator() {
 
-                // TODO
-
-            }
-
-            const_iterator(const const_iterator& other) {
-
-                // TODO
+            const_iterator()
+            {
+                curtree=nullptr;
+				curpos=0;
 
             }
 
-            const_iterator(const iterator& other) {
+            const_iterator(const const_iterator& other)
+            {
 
-                // TODO
+                curtree = other.curtree;
+				block_head = other.block_head;
+				curpos = other.curpos;
 
             }
 
-            // And other methods in iterator, please fill by yourself.
-*/
+            const_iterator(const iterator& other)
+            {
+
+                curtree = other.curtree;
+				block_head = other.block_head;
+				curpos = other.curpos;
+
+            }
+
+            bool modify(const Value& value)
+			{
+				blockhead head;
+				leafnode leaf_node;
+				curtree->readblock(&head, &leaf_node, block_head.pos);
+				leaf_node.val[curpos].second = value;
+				curtree->writeblock(&head, &leaf_node, block_head.pos);
+				return true;
+			}
+			// Return a new iterator which points to the n-next elements
+			const_iterator operator++(int)
+			{
+				// Todo iterator++
+				long int tmp=*this;
+				++curpos;
+				if (curpos>=block_head._size)
+                {
+					char buffer[blocksize];
+					curtree->readmem(buffer,blocksize,block_head.next);
+					memcpy(&block_head,buffer,sizeof(block_head));
+					curpos=0;
+				}
+				return tmp;
+			}
+			const_iterator& operator++() {
+				// Todo ++iterator
+				++curpos;
+				if (curpos>=block_head._size) {
+					char buffer[blocksize];
+					curtree->readmem(buffer,blocksize,block_head.next);
+					memcpy(&block_head,buffer,sizeof(block_head));
+					curpos=0;
+				}
+				return *this;
+			}
+			const_iterator operator--(int)
+			{
+				// Todo iterator--
+				long int tmp=*this;
+				if (curpos==0) {
+					char buffer[blocksize];
+					curtree->readmem(buffer,blocksize,block_head.prev);
+					memcpy(&block_head,buffer,sizeof(block_head));
+					curpos=block_head._size-1;
+				}
+				else --curpos;
+				return tmp;
+			}
+			const_iterator& operator--()
+			{
+				// Todo --iterator
+				if (curpos==0) {
+					char buffer[blocksize];
+					curtree->readmem(buffer,blocksize,block_head.prev);
+					memcpy(&block_head,buffer,sizeof(block_head));
+					curpos=block_head._size-1;
+				}
+				else --curpos;
+
+				return *this;
+			}
+			// Overloaded of operator '==' and '!='
+			// Check whether the iterators are same
+			value_type operator*() const
+			{
+				// Todo operator*, return the <K,V> of iterator
+				if (curpos>=block_head._size) throw invalid_iterator();
+				char buffer[blocksize];
+				curtree->readmem(buffer,blocksize,block_head.pos);
+				leafnode leaf_node;
+				memcpy(&leaf_node,buffer+blockheadsize,sizeof(leaf_node));
+				value_type re(leaf_node.val[curpos].first,leaf_node.val[curpos].second);
+				return re;
+			}
+			bool operator==(const iterator& rhs) const
+			{
+
+				return (curtree==rhs.curtree&&block_head.pos==rhs.block_head.pos&&curpos==rhs.curpos);
+			}
+			bool operator==(const const_iterator& rhs) const
+			{
+
+				return (curtree==rhs.curtree&&block_head.pos==rhs.block_head.pos&&curpos==rhs.curpos);
+			}
+			bool operator!=(const iterator& rhs) const
+			{
+
+				return !(*this==rhs);
+			}
+			bool operator!=(const const_iterator& rhs) const
+			{
+
+				return !(*this==rhs);
+			}
         };
 
         // Default Constructor and Copy Constructor
@@ -471,6 +606,7 @@ public:
 		}
 		~BTree()
 		{
+
 			fclose(fp);
 		}
 
@@ -501,8 +637,8 @@ public:
 				++info._size;
 				info.rootpos=rootpos;
 				writeinfo();
-				pair<iterator,OperationResult> result(begin(),Success);
-				return result;
+				pair<iterator,OperationResult> re(begin(),Success);
+				return re;
 			}
 			//查找正确的节点位置
 			char buffer[blocksize]={0};
@@ -533,30 +669,30 @@ public:
 				curpos=curpos;
 			}
 
-			blockhead info;
-			memcpy(&info,buffer,sizeof(info));
+			blockhead head;
+			memcpy(&head,buffer,sizeof(head));
 			leafnode leafn;
 			memcpy(&leafn,buffer+blockheadsize,sizeof(leafn));
 			for (long int valuepos=0;;++valuepos)
             {
-				if (valuepos<info._size&&(!(leafn.val[valuepos].first<key||leafn.val[valuepos].first>key)))
+				if (valuepos<head._size&&(!(leafn.val[valuepos].first<key||leafn.val[valuepos].first>key)))
 					return pair<iterator,OperationResult>(end(),Fail);
 
-				if (valuepos >= info._size||leafn.val[valuepos].first>key)
+				if (valuepos>=head._size||leafn.val[valuepos].first>key)
 				{
 					//在此结点之前插入
-					if (info._size>=blockpairnum)
+					if (head._size>=blockpairnum)
 					{
-						long int curkey=splitleafnode(curpos,info,leafn);
+						long int curkey=splitleafnode(curpos,head,leafn);
 						if (key>curkey)
                         {
-							curpos=info.next;
-							valuepos-=info._size;
-							readblock(&info,&leafn,curpos);
+							curpos=head.next;
+							valuepos-=head._size;
+							readblock(&head,&leafn,curpos);
 						}
 					}
 
-					for (long int p=info._size-1;p>=valuepos;--p)
+					for (long int p=head._size-1;p>=valuepos;--p)
                     {
 						leafn.val[p+1].first=leafn.val[p].first;
 						leafn.val[p+1].second=leafn.val[p].second;
@@ -564,13 +700,15 @@ public:
 					}
 					leafn.val[valuepos].first=key;
 					leafn.val[valuepos].second=value;
-					++info._size;
-					writeblock(&info,&leafn,curpos);
-					iterator ans;
-					//修改树的基本参数
+					++head._size;
+					writeblock(&head,&leafn,curpos);
+					iterator iter;
+					iter.curtree=this;
+					iter.curpos=valuepos;
+					iter.block_head=head;
 					++info._size;
 					writeinfo();
-					pair<iterator,OperationResult> re(ans,Success);
+					pair<iterator,OperationResult> re(iter,Success);
 					return re;
 				}
 			}
@@ -594,15 +732,63 @@ public:
 
         // Return a iterator to the beginning
 
-        iterator begin() {createfile();iterator no;return no;}
+        iterator begin()
+        {
+            createfile();
+            iterator iter;
+            char buffer[blocksize];
+            readmem(buffer,blocksize,info.headblock);
+            blockhead head;
+            memcpy(&head,buffer,sizeof(head));
+            iter.curtree=this;
+            iter.curpos=0;
+            iter.block_head=head;
+            ++iter;
+            return iter;
+        }
 
-        const_iterator cbegin() const {const_iterator no;return no;}
+        const_iterator cbegin() const
+        {
+            createfile();
+            const_iterator iter;
+            char buffer[blocksize];
+            readmem(buffer,blocksize,info.headblock);
+            blockhead head;
+            memcpy(&head,buffer,sizeof(head));
+            iter.curtree=this;
+            iter.curpos=0;
+            iter.block_head=head;
+            ++iter;
+            return iter;
+        }
 
         // Return a iterator to the end(the next element after the last)
 
-        iterator end() {createfile();iterator no;return no;}
+        iterator end()
+        {
+            createfile();
+            iterator iter;
+            char buffer[blocksize];
+            readmem(buffer,blocksize,info.rearblock);
+            blockhead head;
+            memcpy(&head,buffer,sizeof(head));
+            iter.curtree=this;
+            iter.curpos=0;
+            return iter;
+        }
 
-        const_iterator cend() const {const_iterator no;return no;}
+        const_iterator cend() const
+        {
+            createfile();
+            const_iterator iter;
+            char buffer[blocksize];
+            readmem(buffer,blocksize,info.rearblock);
+            blockhead head;
+            memcpy(&head,buffer,sizeof(head));
+            iter.curtree=this;
+            iter.curpos=0;
+            return iter;
+        }
 
         // Check whether this BTree is empty
 
@@ -688,8 +874,7 @@ public:
 
         size_t count(const Key& key) const
         {
-            if (find(key)==cend()) return 0;
-            else return 1;
+            return find(key)==cend()?0:1;
         }
 
         /**
@@ -706,9 +891,105 @@ public:
 
          */
 
-        iterator find(const Key& key) {return end();}
+        iterator find(const Key& key)
+        {
+            if (empty()) return end();
+            char buffer[blocksize]={0};
+            long int pos=info.rootpos,parent=0;
+            while (true)
+            {
+                readmem(buffer,blocksize,pos);
+                blockhead tmp;
+                memcpy(&tmp,buffer,sizeof(tmp));
+                if (parent!=tmp.parent)
+                {
+                    tmp.parent=parent;
+                    memcpy(buffer,&tmp,sizeof(tmp));
+                    writemem(buffer,blocksize,pos);
+                }
+                if (tmp.type) break;
+                noleafnode node;
+                memcpy(&node,buffer+blockheadsize,sizeof(node));
+                long int childpos=tmp._size-1;
+                while (childpos>0)
+                {
+                    if (node.idx[childpos-1].key<=key)
+                        break;
+                    --childpos;
+                }
+                pos=node.idx[childpos].child;
+            }
+            blockhead head;
+            memcpy(&head,buffer,sizeof(head));
+            leafnode lnode;
+            memcpy(&lnode,buffer+blockheadsize,sizeof(lnode));
+            long int valuepos=0;
+            while (true)
+            {
+                if (valuepos<head._size&&(lnode.val[valuepos].first>=key&&lnode.val[valuepos].first<=key))
+                   {
+                       iterator re;
+                       re.curtree=this;
+                       re.block_head=head;
+                       re.curpos=valuepos;
+                       return re;
+                   }
+                if (valuepos>=head._size||lnode.val[valuepos].first>key)
+                    return end();
+                ++valuepos;
+            }
+            return end();
+        }
 
-        const_iterator find(const Key& key) const {return cend();}
+        const_iterator find(const Key& key) const
+        {
+             if (empty()) return cend();
+            char buffer[blocksize]={0};
+            long int pos=info.rootpos,parent=0;
+            while (true)
+            {
+                readmem(buffer,blocksize,pos);
+                blockhead tmp;
+                memcpy(&tmp,buffer,sizeof(tmp));
+                if (parent!=tmp.parent)
+                {
+                    tmp.parent=parent;
+                    memcpy(buffer,&tmp,sizeof(tmp));
+                    writemem(buffer,blocksize,pos);
+                }
+                if (tmp.type) break;
+                noleafnode node;
+                memcpy(&node,buffer+blockheadsize,sizeof(node));
+                long int childpos=tmp._size-1;
+                while (childpos>0)
+                {
+                    if (node.idx[childpos-1].key<=key)
+                        break;
+                    --childpos;
+                }
+                pos=node.idx[childpos].child;
+            }
+            blockhead head;
+            memcpy(&head,buffer,sizeof(head));
+            leafnode lnode;
+            memcpy(&lnode,buffer+blockheadsize,sizeof(lnode));
+            long int valuepos=0;
+            while (true)
+            {
+                if (valuepos<head._size&&(lnode.val[valuepos].first>=key&&lnode.val[valuepos].first<=key))
+                   {
+                       const_iterator re;
+                       re.curtree=this;
+                       re.block_head=head;
+                       re.curpos=valuepos;
+                       return re;
+                   }
+                if (valuepos>=head._size||lnode.val[valuepos].first>key)
+                    return cend();
+                ++valuepos;
+            }
+            return cend();
+        }
 
     };
 
